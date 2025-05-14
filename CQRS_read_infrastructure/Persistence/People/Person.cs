76b76388c7fb43631bbbfbfb5c29586adf07bd39
alexsoliveira <alexsoliveira@gmail.com>
2025-05-14 10:39:48 -0300

    Criacao classe pessoa

---
+++ CQRS_read_infrastructure/Persistence/People/Person.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CQRS_read_infrastructure.Persistence.People
+{
+    [Flags]
+    public enum PersonClass
+    {
+        Comum,
+        Administrador
+    }
+    public class Person
+    {        
+        public int Id { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PersonClass Class { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Person(int id, PersonClass personClass, string nome, int idade)
+        {
+            if (string.IsNullOrWhiteSpace(nome)) throw new ArgumentNullException("nome");
+
+            Id = id;
+            Class = personClass;
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public Person(PersonClass personClass, string nome, int idade)
+        {
+            Id = -1;
+            Class = personClass;
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Class}:[Class]{this.Class}, [Nome]{this.Nome}. [Idade]{this.Idade}";
+        }
+    }
+}
