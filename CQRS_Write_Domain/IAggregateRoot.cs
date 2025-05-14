8c234fec95264975d26155ebc5c83eb5d92fd0fa
alexsoliveira <alexsoliveira@gmail.com>
2025-05-14 13:29:09 -0300

    Criacao do agrgregateroot

---
+++ CQRS_Write_Domain/IAggregateRoot.cs
@@ -0,0 +1,23 @@
+﻿using CQRS_Write_Domain.Events;
+
+namespace CQRS_Write_Domain
+{
+    public interface IAggregateRoot<T> : IggregateRoot
+    {
+        T Id { get; }
+    }
+
+    public interface IggregateRoot
+    {
+        object GetId();
+        int Version { get; }
+
+        IEnumerable<IEvent> GetUncommittedChanges();
+
+        void MarkChangesAsCommitted();
+
+        void LoadsFromHistory(IEnumerable<IEvent> history);
+
+        void ApplyChange(IEvent @event, bool isNew = true);
+    }
+}
