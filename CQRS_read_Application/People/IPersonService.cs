120d1e86c10737c94fc9acffa3b7064da88300b6
alexsoliveira <alexsoliveira@gmail.com>
2025-05-14 10:48:32 -0300

    Criacao serviço pessoa

---
+++ CQRS_read_Application/People/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using CQRS_read_infrastructure.Persistence.People;
+
+namespace CQRS_read_Application.People
+{
+    public interface IPersonService : IApplicationService<Person>
+    {
+        Person Find(int id);
+        IQueryable<Person> GetByName(string name);
+        IQueryable<Person> GetAll();
+        void Insert(Person person);
+        void Update(Person person); 
+        void Delete(int id);
+    }
+}
---
+++ CQRS_read_Application/People/PersonService.cs
@@ -0,0 +1,44 @@
+﻿using CQRS_read_infrastructure.Persistence.People;
+
+namespace CQRS_read_Application.People
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IPersonRepository personRepository;
+
+        public PersonService(IPersonRepository personRepository)
+        {
+            this.personRepository = personRepository;
+        }
+
+        public void Delete(int id)
+        {
+            personRepository.Delete(id);
+        }
+
+        public Person Find(int id)
+        {
+            return personRepository.Find(id);
+        }
+
+        public IQueryable<Person> GetAll()
+        {
+            return personRepository.Get();
+        }
+
+        public IQueryable<Person> GetByName(string name)
+        {
+            return personRepository.Get(p => p.Nome.ToUpper().Contains(name.ToUpper()));
+        }
+
+        public void Insert(Person person)
+        {
+            personRepository.Insert(person);
+        }
+
+        public void Update(Person person)
+        {
+            personRepository.Update(person);
+        }
+    }
+}
